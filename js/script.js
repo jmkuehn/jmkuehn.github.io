26c4472d5b860df4131c01d49bb3aa4c8543f7df
Josh Kuehn <jmkuehn1@aol.com>
2016-04-07 23:42:08 -0400

    updated geopattern to change hourly

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 var options = new Object();
 options.generator='hexagons';
-var pattern = GeoPattern.generate(navigator.userAgent, options);
+var pattern = GeoPattern.generate((new Date().getTime()/(1000*60*60)|0).toString(), options);
 $('html').css('background-image', pattern.toDataUrl());
